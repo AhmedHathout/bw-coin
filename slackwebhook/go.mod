80c3f8dd82a91ac69dedaa256ec1cf6b3a017927
bw-bmbarga <bmbarga@brandwatch.com>
2019-06-04 21:53:30 +0200

    fetch user data from mongodb

--- slackwebhook/go.mod
+++ slackwebhook/go.mod
@@ -1,3 +1,5 @@
 module github.com/BrandwatchLtd/bw-coin/slackwebhook
 
 go 1.12
+
+require gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
--- slackwebhook/main.go
+++ slackwebhook/main.go
@@ -4,16 +4,63 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"gopkg.in/mgo.v2"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
 const (
 	cPort      = "3030"
-	cBlockPath = "./res/button_block.json"
+	cBlockPath = "./res/listelement.json"
 )
+
+type sUserDB struct {
+	Login     string
+	URL       string
+	AvatarURL string
+	Type      string
+	ID        int
+	Coins     int
+}
+
+const (
+	cDBName     = "bw-coin"
+	cDBUsername = "elhmn"
+	cDBPassword = "mongobeti" //Later fetch password using os.Getenv()
+)
+
+var gSession *mgo.Session
+
+func createDatabase() *mgo.Session {
+	mongoDBDialInfo := &mgo.DialInfo{
+		Addrs:    []string{"localhost"},
+		Database: cDBName,
+		Username: cDBUsername,
+		Password: cDBPassword,
+	}
+	session, err := mgo.DialWithInfo(mongoDBDialInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+	return session
+}
+
+func getUserData() []sUserDB {
+
+	usersCollection := gSession.DB(cDBName).C("users")
+	var users []sUserDB
+	if err := usersCollection.Find(nil).Sort("-coins").All(&users); err != nil {
+		panic(err)
+	}
+	return users
+}
 
 func root(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("bw-coin slack made by @elhmn"))
@@ -38,7 +85,22 @@ func bwcoinHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := "{" + "\"blocks\"" + ":" + string(block) + "}"
+	users := getUserData()
+	blocks := ""
+	for i, user := range users {
+		tmp := strings.ReplaceAll(string(block), "[[login]]", user.Login)
+		tmp = strings.ReplaceAll(string(tmp), "[[login_url]]", user.URL)
+		tmp = strings.ReplaceAll(string(tmp), "[[avatar_url]]", user.AvatarURL)
+		tmp = strings.ReplaceAll(string(tmp), "[[coins]]", strconv.Itoa(user.Coins))
+		tmp = strings.ReplaceAll(string(tmp), "[[rank]]", strconv.Itoa(i+1))
+		blocks += tmp
+		if len(users)-1 != i {
+			blocks += ","
+		}
+	}
+
+	data := "{" + "\"blocks\"" + ":[" + string(blocks) + "]}"
+	fmt.Println(data)
 
 	w.Write([]byte(data))
 }
@@ -85,11 +147,19 @@ func interactionHandler(w http.ResponseWriter, r *http.Request) {
 
 func run() {
 	fmt.Println("Server started at port :", cPort)
+	if gSession == nil {
+		fmt.Println("Setting up Database session...")
+		gSession = createDatabase()
+		fmt.Println("Database successfully setup !")
+	}
 	http.HandleFunc("/", root)
 	http.HandleFunc("/bwcoin", bwcoinHandler)
 	http.HandleFunc("/interaction", interactionHandler)
 	if err := http.ListenAndServe(":"+cPort, nil); err != nil {
 		log.Fatal(err)
+	}
+	if gSession != nil {
+		gSession.Close()
 	}
 }
 
