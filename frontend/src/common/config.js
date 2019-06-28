b01655027a1911cf8bf4d69174b108c065c004bc
bw-bmbarga <bmbarga@brandwatch.com>
2019-06-28 15:21:12 +0200

    Fetch user data from DB

---
+++ frontend/src/common/config.js
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*  config.js                                                                 */
+/*                                                                            */
+/*   By: elhmn <www.elhmn.com>                                                */
+/*             <nleme@live.fr>                                                */
+/*                                                                            */
+/*   Created:                                                 by elhmn        */
+/*   Updated: Fri Jun 28 14:51:50 2019                        by bmbarga      */
+/*                                                                            */
+/* ************************************************************************** */
+
+export const Config = {
+	apiDomainName: "localhost:8080",
+};
+
+export default Config;
--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -6,38 +6,69 @@
 /*             <nleme@live.fr>                                                */
 /*                                                                            */
 /*   Created:                                                 by elhmn        */
-/*   Updated: Fri Jun 28 13:06:33 2019                        by bmbarga      */
+/*   Updated: Fri Jun 28 15:19:57 2019                        by bmbarga      */
 /*                                                                            */
 /* ************************************************************************** */
 
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoins } from '@fortawesome/free-solid-svg-icons';
-import { List, Avatar, Row, Col } from 'antd';
+import { List, Avatar, Row, Col, message } from 'antd';
+import { JsonRequest } from '../common/DataHandler';
+import { Config } from '../common/config';
 
-const UserDetail = () => {
+const UserDetail = ({ coins }) => {
   return (
     <Row type="flex" justify="start" align="start" gutter={10}>
       <Col>
-        190 <FontAwesomeIcon icon={faCoins} />
+        {coins} <FontAwesomeIcon icon={faCoins} />
       </Col>
-      <Col>| 7 PR reviewed</Col>
+      <Col>| (n) PR reviewed</Col>
     </Row>
   );
 };
 
-const data = [
-  {
-    title: 'Element',
-    content: <UserDetail />
-  },
-  {
-    title: 'Boris Mbarga',
-    content: <UserDetail />
-  }
-];
-
 const UserList = () => {
+  const [userData, setUserData] = useState(null);
+
+  const getDataSource = useCallback(() => {
+    return userData ? userData.map(user => ({
+        ...user,
+        content: <UserDetail coins={user.coins}/>
+    })) : [];
+  }, [userData]);
+
+  const getUserData = useCallback(() => {
+    const onsuccess = response => {
+      const data = (response && JSON.parse(response)) || [];
+      message.success('user data updated successfully');
+      setUserData(data);
+    };
+
+    const onerror = () => {
+      const user = this.props.store.user || {};
+
+      this.setState({
+        user,
+        prevuser: user
+      });
+      message.error('user data failed to update');
+    };
+
+    JsonRequest({
+      url: `http://${Config.apiDomainName}/users`,
+      method: 'get',
+      onsuccess,
+      onerror
+    });
+  }, [userData]);
+
+  useEffect(() => {
+      if (userData === null) {
+        getUserData();
+      }
+  });
+
   return (
     <List
       itemLayout="vertical"
@@ -49,14 +80,14 @@ const UserList = () => {
         pageSize: 10
       }}
       bordered
-      dataSource={data}
+      dataSource={getDataSource()}
       renderItem={item => (
         <List.Item>
           <List.Item.Meta
             avatar={
-              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              <Avatar src={item.avatar_url} size="large"/>
             }
-            title={<a href="userurl">{item.title}</a>}
+            title={<a href={item.url}>{item.login}</a>}
           />
           {item.content}
         </List.Item>
